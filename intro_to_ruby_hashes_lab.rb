b134bd0a64022ba86b8b2839fb83371e56576be5
Duncan Uruchima <duruchima1@gmail.com>
2020-02-28 21:08:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-	{:railroads => {:pieces => 4, :rent_in_dollars => {:boardwalk => "$200", atlantic: "$100", pennsylvania: "$150", conneticut: "$50"}, :names => {}}}
+	{:railroads => {:pieces => 4, :rent_in_dollars => {:boardwalk => "$200", atlantic: "$100", pennsylvania: "$150", conneticut: "$50"}, :names => {player1: "Duncan", player2: "Stephanie", player3: "Peter", Player4: "Alba"}}}
 end
 
 def monopoly_with_fourth_tier
